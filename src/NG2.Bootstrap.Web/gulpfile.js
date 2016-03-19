94f5c45fd0c2192c22c6237e36487917cae15092
Jose Fajardo <fajardo_jf@hotmail.com>
2016-03-20 08:58:04 +1100

    wwwroot now populated from static files via gulp tasks

--- src/NG2.Bootstrap.Web/gulpfile.js
+++ src/NG2.Bootstrap.Web/gulpfile.js
@@ -1,45 +1,92 @@
-﻿/// <binding Clean='clean' />
+﻿/// <binding AfterBuild='compile, copy, min' Clean='clean' />
 "use strict";
 
 var gulp = require("gulp"),
+    install = require("gulp-install"),
+    del = require("del"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
+    replace = require("gulp-replace-task"),
     uglify = require("gulp-uglify");
 
 var paths = {
-    webroot: "./wwwroot/"
+    webroot: "./wwwroot/",
+    staticRoot: "./_staticFiles/"
 };
 
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
 
+
+//==========
+//Bound tasks
+//==========
+
+gulp.task("min", [""]);
+gulp.task("copy", ["copy:static"]);
+gulp.task("compile", [""]);
+gulp.task("clean", ["clean:js", "clean:css", "clean:libs", "clean:themes", "clean:assets", "clean:root"]);
+gulp.task("refresh", [""]);
+
+gulp.task("default", [""]);
+
+
+
+//==========
+//clean
+//==========
 gulp.task("clean:js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
+    return del([paths.webroot + "js/**/*"]);
 });
 
 gulp.task("clean:css", function (cb) {
-    rimraf(paths.concatCssDest, cb);
+    return del([paths.webroot + "css/**/*"]);
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
+gulp.task("clean:libs", function (cb) {
+    return del([paths.webroot + "libs/**/*"]);
 });
 
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
+gulp.task("clean:themes", function (cb) {
+    return del([paths.webroot + "themes/**/*"]);
 });
 
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task("clean:assets", function (cb) {
+    return del([paths.webroot + "assets/**/*"]);
+});
+
+gulp.task("clean:root", function (cb) {
+    return del([paths.webroot + "**/*"]);
+});
+
+
+
+
+
+
+
+
+
+//==========
+//COPY 
+//==========
+
+
+gulp.task("copy:static", function (cb) {
+    
+    gulp.src(["_staticFiles/root/**/*"])
+        .pipe(gulp.dest("wwwroot/"));
+
+    gulp.src(["_staticFiles/assets/**/*"])
+        .pipe(gulp.dest("wwwroot/assets/"));
+
+    gulp.src(["_staticFiles/js/**/*"])
+        .pipe(gulp.dest("wwwroot/js/"));
+
+    gulp.src(["_staticFiles/css/**/*"])
+        .pipe(gulp.dest("wwwroot/css/"));
+
+    cb();
+});
